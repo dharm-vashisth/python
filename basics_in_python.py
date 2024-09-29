7b27af7a9a64c8870f802baa3d81311f8ae7b2d3
Dharm Vashisth <dharmvashisth5@gmail.com>
2024-09-29 12:01:03 +0530

    Program to buy 2 tickets from in-built wallet.

--- basics_in_python.py
+++ basics_in_python.py
@@ -1,2 +1,23 @@
 # first line of python directory
 print('Hello World')
+
+
+# buying movie ticket from wallet.
+def purchase_tickets(wallet, expense):
+    if wallet < expense:
+        print("It is better to go for a walk as it is good for our health")
+
+    else:
+        wallet -= expense
+        print("Hurray!! you have purchased two movie tickets. Enjoy the movie!!!")
+
+    print(f"Wallet Balance: {round(wallet, 4)}")
+    return wallet
+
+
+wallet_balance = 1000 # 1000 inr
+movie1 = float(input("Pls provide the price for movie ticket 1: "))
+movie2 = float(input("Pls provide the price for movie ticket 2: "))
+movie_expense = movie1 + movie2
+print("Doing the science....")
+wallet_balance = purchase_tickets(wallet_balance, movie_expense)
