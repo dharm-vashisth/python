4f1225f86f6e571bdfffa79f10fc0aede2a9ee63
Dharm Vashisth <dharmvashisth5@gmail.com>
2024-10-02 00:05:13 +0530

    list comprehension.

--- iterative_loops.py
+++ iterative_loops.py
@@ -1,3 +1,9 @@
+# list of numbers
 items = [1,2,3,4,5,6]
 for item in items:
     print(item)
+
+# even numbers out of list using list comprehension
+number = range(244)
+even= [num for num in number if num%2==0]
+print(even)
