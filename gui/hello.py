599ff3f6725ee42765f35d00635c61fe83ae22c8
Dharm Vashisth <dharmvashisth5@gmail.com>
2024-10-05 11:35:28 +0530

    Introduction to Python GUI.

---
+++ gui/hello.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# hello tkinter by Dharm Vashisth
+
+from tkinter import *
+
+root = Tk()
+Label(root, text= "Hello Python GUI").pack()
+root.mainloop()
---
+++ gui/readme.md
@@ -0,0 +1,5 @@
+***
+## Learning Pyhton GUI
+***
+### Tkinter programmes with Dharm Vashisth
+- Hello Python GUI
