a4502187d003b37310881e4d5e4f4a713539a068
Dharm Vashisth <dharmvashisth5@gmail.com>
2024-10-10 00:42:48 +0530

    getter methods for class using @property decorator.

---
+++ intermediate_python_workshop/basic_bank_class_with_getter.py
@@ -0,0 +1,27 @@
+# author: Dharm Vashisth
+# date: 2024-10-10
+
+class BankAccount:
+    def __init__(self):
+        self._balance = 0
+
+    #  getter with the attribute name balance.
+    #  As we have _balance in class, now we could access it using .balance also.
+    @property
+    def balance(self):
+        return self._balance
+
+    def deposit(self, amount):
+        self._balance+=amount
+    def withdraw(self,amount):
+        if amount<self.balance:
+            self._balance-=amount
+
+def main():
+    bank = BankAccount()
+    bank.deposit(100)
+    print("Deposited 100 in your bank account.")
+    print("Balance:",bank.balance) # accessed by getter method
+
+if __name__=="__main__":
+    main()
