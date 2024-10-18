f8ee47a713c83698c8a1c428ae301ef196125336
Dharm Vashisth <dharmvashisth5@gmail.com>
2024-10-18 23:18:51 +0530

    added one more level exception i.e., divide by zero.

--- object_oriented_programming/exception_handling.py
+++ object_oriented_programming/exception_handling.py
@@ -15,10 +15,15 @@
 
 def main():
     try:
-        number = int(input("Enter an integer number"))
+        number = int(input("Enter an integer number: "))
         print("Number entered by you is ",number)
+        number2 = int(input("Enter an integer number 2: "))
+        print("Number2 entered by you is ",number2)
+        print("Division of number by number 2 is ",number/number2)
+    except ZeroDivisionError as ze:
+        print("Got the Divisible by Zero exception!!!\n",ze)
     except ValueError as ve:
-        print("Got the value error!!!\n",ve)
+        print("Got the value error!!!\nNot getting an integer value\n",ve)
     except Exception as e:
         print("Exception caught!!!\nError message: ",e)
 
