679b25beae836595f08aadec75f9cdd351908c8a
Dharm Vashisth <dharmvashisth5@gmail.com>
2024-10-18 23:42:07 +0530

    Dictionary comprehension.

--- comprehensions.py
+++ comprehensions.py
@@ -19,6 +19,13 @@ def main():
     print('Even numbers under 100 using list comprehension method are shown below', end='\n\n')
     print(_number_list)
 
+    _random_list = ['value '+str(a) for a in range(len(_number_list))]
+    print(_random_list)
+
+    #dictionary comprehension
+    _my_dict = {(i,j) for i in _number_list  for j in _random_list}
+    print(_my_dict)
+
 
 if __name__ == "__main__":
     main()
