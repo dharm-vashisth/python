a3d30ad8c0499d153bd5b1cee673d920521a9add
Dharm Vashisth <dharmvashisth5@gmail.com>
2024-10-18 23:45:40 +0530

    set comprehension.

--- comprehensions.py
+++ comprehensions.py
@@ -16,15 +16,21 @@
 def main():
     # list comprehensions
     _number_list = [a for a in range(1,100) if a%2==0 ]
-    print('Even numbers under 100 using list comprehension method are shown below', end='\n\n')
-    print(_number_list)
+    print("List Comprehension")
+    print('Even numbers under 100 using list comprehension method are shown below')
+    print(_number_list, end='\n\n')
 
-    _random_list = ['value '+str(a) for a in range(len(_number_list))]
-    print(_random_list)
+    # set comprehension
+    _random_list = ['value ' + str(a) for a in range(len(_number_list))]
+    print("Set Comprehension")
+    _my_set = {i for i in _random_list}
+    print(_my_set, end='\n\n')
+
 
     #dictionary comprehension
+    print("Dictionary Comprehension")
     _my_dict = {(i,j) for i in _number_list  for j in _random_list}
-    print(_my_dict)
+    print(_my_dict, end='\n\n')
 
 
 if __name__ == "__main__":
